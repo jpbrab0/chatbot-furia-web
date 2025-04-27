aec1fb3b07e8e4b12ad4b15bf57f308ef19dda16
João Pedro Resende <jpresende502@gmail.com>
2025-04-27 00:35:26 -0300

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header } from './components/layout/header'
+import { Chat } from './components/layout/chat'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <Chat />
+      </div>
+    </>
+  )
+}
+
+export default App
